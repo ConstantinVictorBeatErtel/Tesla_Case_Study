c378eb11a33a8d9a7163ec23a358bf001b69f985
Sanchit Ram Arvind <sanchitram@gmail.com>
2025-10-13 21:48:19 -0700

    live data

--- live_data.py
+++ live_data.py
@@ -7,15 +7,13 @@ load_dotenv()
 
 api_key = os.getenv("FRED_API_KEY")
 url = "https://api.stlouisfed.org/fred/series/observations"
-params = {
-    "series_id": "FEDFUNDS",
-    "api_key": api_key,
-    "file_type": "json",
-}
-
-response = get(url, params=params)
 
 
 def get_fed_funds_rate():
+    params = {
+        "series_id": "FEDFUNDS",
+        "api_key": api_key,
+        "file_type": "json",
+    }
     response = get(url, params=params)
     return response.json()
